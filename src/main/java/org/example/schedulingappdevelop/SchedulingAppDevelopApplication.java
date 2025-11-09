06212b343cb41c2d3b6dafa87d48cf43c05eb9ac
remnantcjy <remnantcjy@gmail.com>
2025-11-09 17:18:51 +0900

    Initial commit

---
+++ src/main/java/org/example/schedulingappdevelop/SchedulingAppDevelopApplication.java
@@ -0,0 +1,13 @@
+package org.example.schedulingappdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingAppDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingAppDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/schedulingappdevelop/SchedulingAppDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schedulingappdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingAppDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
